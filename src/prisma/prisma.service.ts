d1d069f505e557801c9e4a6591a75689ce217a32
marcebenitez2 <marcebenitez0607@gmail.com>
2026-05-07 20:53:12 -0300

    chore: first step

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from '../../generated/prisma'
+import { PrismaClient } from 'generated/prisma/client'
+import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!)
+    super({ adapter })
+  }
+
   async onModuleInit() {
-    await this.$connect()
+    try {
+      await this.$connect()
+      await this.$queryRaw`SELECT 1`
+      console.log('✅ Prisma connected to MySQL')
+    } catch (error) {
+      console.error('❌ Prisma connection error:', error)
+      throw error
+    }
   }
 }
