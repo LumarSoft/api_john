3dc61c8a9f819a50115825dfe4c0748fe7fb7ba7
lquaroni <lucas.quaroni@gmail.com>
2026-06-09 00:08:37 -0300

    refactor: extract AuthenticatedRequest interface to common types and update controller usage

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, Param, ParseIntPipe, Request, UseGuards } from '@nestjs/common'
 import { ClientsService } from './clients.service'
 import { ClientAuthGuard } from '../auth/client-auth.guard'
-
-interface ClientRequest {
-  user: { id: number; producerId: number; type: string }
-}
+import { AuthenticatedRequest } from '../common/types/authenticated-request.type'
 
 @Controller('clients')
 @UseGuards(ClientAuthGuard)
@@ -12,12 +9,12 @@ export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Get('me/polizas')
-  getMyPolizas(@Request() req: ClientRequest) {
+  getMyPolizas(@Request() req: AuthenticatedRequest) {
     return this.clientsService.findPolizas(req.user.id, req.user.producerId)
   }
 
   @Get('me/polizas/:id')
-  getPoliza(@Param('id', ParseIntPipe) id: number, @Request() req: ClientRequest) {
+  getPoliza(@Param('id', ParseIntPipe) id: number, @Request() req: AuthenticatedRequest) {
     return this.clientsService.findPolizaById(id, req.user.id, req.user.producerId)
   }
 }
